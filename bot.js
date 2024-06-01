c96c499814c9e37e4173a20900a716fca319d8f3
Ian <ian.kellogg001@gmail.com>
2024-06-01 15:58:57 -0800

    Added random delay before messages to appear more natural

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
+const { setTimeout } = require('timers/promises');
 const WebSocket = require('ws');
 
 let socket;
 
-let oAuth;
+let oAuth; // TODO: Put these variables in an object because why wouldn't they be?
 let nick;
-let channel;
+let channel; // No reason this should ever be anything other than 'dunkorslam'
+let minimum; // Minimum delay (in seconds) before sending spam message
+let maximum; // Maximum delay, should never be greater than 30 (because there's no point)
 let messages;
 
 let lastIndex = 0;
+let messageQueued = false;
 
 loadConfig(registerSocket);
 console.log(oAuth)
@@ -21,13 +25,21 @@ function loadConfig(_callback) {
     oAuth = config.oAuth;
     nick = config.nick;
     channel = config.channel;
+    minimum = config.minimum;
+    maximum = config.maximum;
     messages = config.messages;
 
     console.log(oAuth)
     console.log(nick)
 
+    // Validation
     if (messages.length <= 1) {
         console.log('Error: Message array must have at least two entries');
+        process.exit(1);
+    }
+
+    if (minimum < 0 || maximum > 30 || maximum < minimum) {
+        console.error("Error: Minimum/maximum must be between 0 and 30");
         process.exit(1);
     }
 
@@ -59,11 +71,25 @@ function registerSocket() {
             console.log('')
             console.log('-------------------------------------------')
 
-            socket.send(`PRIVMSG #${channel} :${messages[lastIndex]}`);
-
-            if (++lastIndex >= messages.length) {
-                lastIndex = 0;
-            }
+            queueMessage(); // Don't care about promise lmao
         }
     });
+}
+
+async function queueMessage() {
+    if (messageQueued) {
+        return; // Don't queue another message if there's already one waiting (to prevent excessive spam)
+    }
+
+    messageQueued = true;
+
+    await setTimeout(random(minimum, maximum));
+
+    socket.send(`PRIVMSG #${channel} :${messages[lastIndex]}`);
+    lastIndex = (lastIndex + 1 > messages.length) ? lastIndex + 1 : 0;
+    messageQueued = false;
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
 }
